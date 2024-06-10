f3a15ced382134f07feb313d8a5775b6f8c32559
carshy <mussocollins@gmail.com>
2024-06-10 16:41:30 +0300

    Fix eslint errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Provider } from 'react-redux';
 import { Navbar } from './components';
 import './App.scss';
+import store from './redux/configureStore';
 
 const App = () => {
   return (
     <motion.div className="app">
-      <Navbar />
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
     </motion.div>
   );
 };
