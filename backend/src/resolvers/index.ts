9e2ddc8a42280d1f1011525fb0baf9f31a833269
carshy <mussocollins@gmail.com>
2024-06-18 13:05:09 +0300

    Configured search functionality

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -3,5 +3,11 @@ import { booksData } from '../data/books';
 export const resolvers = {
   Query: {
     books: () => booksData,
+    searchBooks: (_: any, { query }: { query: string }) => {
+      const lowerCaseQuery = query.toLowerCase();
+      return booksData.filter(book =>
+        book.title.toLowerCase().includes(lowerCaseQuery)
+      );
+    },
   },
 };
