b6704e53768e2c95f324c20c9b0c55c63ef39648
carshy <mussocollins@gmail.com>
2024-06-19 22:02:28 +0300

    Add start loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Navbar } from './components';
@@ -7,21 +7,32 @@ import './App.scss';
 import store from './redux/configureStore';
 
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById('spinner');
+
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = 'none';
+      setLoading(false);
+    }, 3000);
+  }
 
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/books" element={<SearchList />} />
-            {/* <Route path="/books/:bookId" element={<ShowBook />} /> */}
-            {/* <Route path="/books/:book" element={<Books />} /> */}
-          </Routes>
-        </div>
-      </Router>
-    </Provider>
+    !loading && (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/books" element={<SearchList />} />
+              {/* <Route path="/books/:bookId" element={<ShowBook />} /> */}
+              {/* <Route path="/books/:book" element={<Books />} /> */}
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
+    )
   );
 };
 
