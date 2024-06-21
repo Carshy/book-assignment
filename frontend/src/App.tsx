de05f5e406ba85feebc58ef25b0dab9729499e06
carshy <mussocollins@gmail.com>
2024-06-21 06:49:22 +0300

    Fix errors in apps.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,42 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { Navbar } from './components';
+// import { Home, SearchList } from './containers';
+// import './App.scss';
+// import store from './redux/configureStore';
+
+// const App: React.FC = () => {
+//   const [loading, setLoading] = useState(true);
+//   const spinner = document.getElementById('spinner');
+
+//   if (spinner) {
+//     setTimeout(() => {
+//       spinner.style.display = 'none';
+//       setLoading(false);
+//     }, 3000);
+//   }
+
+//   return (
+//     !loading && (
+//       <Provider store={store}>
+//         <Router>
+//           <div className="App">
+//             <Navbar />
+//             <Routes>
+//               <Route path="/" element={<Home />} />
+//               <Route path="/books" element={<SearchList />} />
+//             </Routes>
+//           </div>
+//         </Router>
+//       </Provider>
+//     )
+//   );
+// };
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Navbar } from './components';
@@ -8,29 +46,33 @@ import store from './redux/configureStore';
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
-  const spinner = document.getElementById('spinner');
 
-  if (spinner) {
-    setTimeout(() => {
-      spinner.style.display = 'none';
-      setLoading(false);
-    }, 3000);
+  useEffect(() => {
+    const spinner = document.getElementById('spinner');
+    if (spinner) {
+      setTimeout(() => {
+        spinner.style.display = 'none';
+        setLoading(false);
+      }, 3000);
+    }
+  }, []);
+
+  if (loading) {
+    return null;
   }
 
   return (
-    !loading && (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/books" element={<SearchList />} />
-            </Routes>
-          </div>
-        </Router>
-      </Provider>
-    )
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<SearchList />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
